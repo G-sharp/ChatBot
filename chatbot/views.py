b4913c84e38bb5c25b86ac68e83a5124d1d563b3
graysonnet <graysonnet@qq.com>
2019-07-17 14:54:05 +0800

    :sparkles: Add SessionID, Some bug fixed.

--- chatbot/views.py
+++ chatbot/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 # -*- coding: utf-8 -*-
 import json
+import datetime
 from django.http import HttpResponse
 from chatbot import aimlKernel
 
@@ -10,6 +11,8 @@ def chat(request):
     dic = {}
     if request.method == 'GET':
         dic['botResponse'] = aimlKernel.k.respond(request.GET.get('ask', '无语'),request.GET.get('sessionid','test')).replace(' ', '')
+        dic['time'] = datetime.datetime.now().strftime(('%Y-%m-%d %H:%M:%S'))
+        dic['sessionid'] = request.GET.get('sessionid','test')
         return HttpResponse(json.dumps(dic, ensure_ascii=False))
     else:
         dic['message'] = u'方法错误'
